7822b0cbbe70d751c95b0ca5ea4dc7cbfc445793
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2026-03-16 17:17:14 +0100

    Huge refactoring over methods: in some methods a new object is returned instead of the same and add length attribute for better readability + working on sort method

--- JSArray.java
+++ JSArray.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,6 +12,7 @@ public class JSArray<T extends Object> {
 
     private T[] elements;
     private final int MAX_CAPACITY = Integer.MAX_VALUE - 1;
+    private int length;
 
     private class JSArrayIterator implements Iterator<T> {
 
@@ -24,12 +24,12 @@ public class JSArray<T extends Object> {
 
         @Override
         public boolean hasNext() {
-            return this.index < elements.length;
+            return this.index < length;
         }
 
         @Override
         public T next() {
-            if (this.hasNext() && this.index < elements.length) {
+            if (this.hasNext() && this.index < length) {
                 return elements[this.index++];
             }
             return null;
@@ -50,51 +50,61 @@ public class JSArray<T extends Object> {
         return dimension;
     }
 
-    public JSArray(@SuppressWarnings("unchecked") T... array) throws SizeLimitExceededException {
-        if (array.length > MAX_CAPACITY) {
+    @SuppressWarnings("unchecked")
+    public JSArray(int _length) throws SizeLimitExceededException {
+        if (length > MAX_CAPACITY) {
             throw new SizeLimitExceededException("Cannot exceed max size: " + MAX_CAPACITY);
         }
-        this.elements = array;
+        this.length = _length;
+        this.elements = (T[]) new Object[_length];
     }
 
-    public static String[] from(String text) {
-        return text.split("");
+    public JSArray(@SuppressWarnings("unchecked") T... values) throws SizeLimitExceededException {
+        if (values.length > MAX_CAPACITY) {
+            throw new SizeLimitExceededException("Cannot exceed max size: " + MAX_CAPACITY);
+        }
+        this.elements = values;
+        this.length = this.elements.length;
     }
 
-    public static <T extends Object> T[] from(T[] array, Function<T, ?> mapper) {
-        T[] out = Arrays.copyOf(array, array.length);
+    public static JSArray from(String text) throws SizeLimitExceededException {
+        return new JSArray<>(text.split(""));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Object> JSArray from(JSArray array, Function<T, ?> mapper) throws SizeLimitExceededException {
+        T[] out = (T[]) Arrays.copyOf(array.toPrimitiveArray(), array.length);
         for (int i = 0; i < array.length; i++) {
-            out[i] = (T) mapper.apply(array[i]);
+            out[i] = (T) mapper.apply((T) array.toPrimitiveArray()[i]);
         }
 
-        return out;
+        return new JSArray<>(out);
     }
 
-    public T[] get() {
+    public T[] toPrimitiveArray() {
         return this.elements;
     }
 
     public T at(int index) {
-        if (index >= this.elements.length) {
+        if (index >= this.length) {
             throw new IndexOutOfBoundsException();
         }
         return this.elements[index];
     }
 
     private T[] concat(T[] array) {
-        T[] out = Arrays.copyOf(this.elements, this.elements.length + array.length);
+        T[] out = Arrays.copyOf(this.elements, this.length + array.length);
 
         for (int i = 0; i < array.length; i++) {
-            out[i + this.elements.length] = (T) array[i];
+            out[i + this.length] = (T) array[i];
         }
 
         return out;
     }
 
-    public JSArray concat(JSArray array) {
-        this.elements = this.concat((T[]) array.get());
-
-        return this;
+    @SuppressWarnings("unchecked")
+    public JSArray concat(JSArray array) throws SizeLimitExceededException {
+        return new JSArray<>(this.concat((T[]) array.toPrimitiveArray()));
     }
 
     /*
@@ -104,7 +114,7 @@ public class JSArray<T extends Object> {
      * console.log([1, 2, 3, 4, 5].copyWithin(0, 3, 4));
      * // [4, 2, 3, 4, 5]
      */
-    public JSArray copyWithin(int target, int start, int end) {
+    public JSArray copyWithin(int target, int start, int end) throws SizeLimitExceededException {
         if (target > end) {
             throw new IllegalArgumentException("target index must be lower than end index");
         }
@@ -114,20 +124,21 @@ public class JSArray<T extends Object> {
         }
 
         ArrayDeque<T> range = new ArrayDeque<>();
+        T[] out = Arrays.copyOf(this.elements, this.length);
 
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < out.length; i++) {
             if (i >= start && i < end) {
-                range.add(this.elements[i]);
+                range.add(out[i]);
             }
         }
 
         int i = target;
-        while (!range.isEmpty() && i < this.elements.length) {
-            this.elements[i] = range.pop();
+        while (!range.isEmpty() && i < out.length) {
+            out[i] = range.pop();
             i++;
         }
 
-        return this;
+        return new JSArray<>(out);
     }
 
     public Iterator<T> entries() {
@@ -137,7 +148,7 @@ public class JSArray<T extends Object> {
     public boolean every(BiFunction<T, Integer, Boolean> predicate) {
         boolean isValid = true;
 
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < this.length; i++) {
             isValid = predicate.apply(this.elements[i], i).booleanValue();
             if (!isValid) {
                 return isValid;
@@ -147,38 +158,41 @@ public class JSArray<T extends Object> {
         return isValid;
     }
 
-    public JSArray fill(T value, int start, int end) {
+    public JSArray fill(T value, int start, int end) throws SizeLimitExceededException {
         if (start >= end) {
             throw new IllegalArgumentException("start index must be lower than end index");
         }
 
-        if (start >= this.elements.length || end > this.elements.length) {
+        if (start >= this.length || end > this.length) {
             throw new IllegalArgumentException("indices lust be lower than array size");
         }
 
+        T[] out = Arrays.copyOf(this.elements, this.length);
+
         for (int i = start; i < end; i++) {
-            this.elements[i] = value;
+            out[i] = value;
         }
 
-        return this;
+        return new JSArray<>(out);
     }
 
-    public JSArray filter(BiPredicate<T, Integer> predicate) {
+    public JSArray filter(BiPredicate<T, Integer> predicate) throws SizeLimitExceededException {
         ArrayList<T> filtered = new ArrayList<>();
 
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < this.length; i++) {
             if (predicate.test(this.elements[i], i)) {
                 filtered.add(this.elements[i]);
             }
         }
 
-        this.elements = (T[]) filtered.toArray(new Object[filtered.size()]);
+        @SuppressWarnings("unchecked")
+        T[] result = (T[]) filtered.toArray(new Object[filtered.size()]);
 
-        return this;
+        return new JSArray<>(result);
     }
 
     public T find(BiPredicate<T, Integer> predicate) {
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < this.length; i++) {
             if (predicate.test(this.elements[i], i)) {
                 return this.elements[i];
             }
@@ -188,7 +202,7 @@ public class JSArray<T extends Object> {
     }
 
     public int findIndex(BiPredicate<T, Integer> predicate) {
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < this.length; i++) {
             if (predicate.test(this.elements[i], i)) {
                 return i;
             }
@@ -198,7 +212,7 @@ public class JSArray<T extends Object> {
     }
 
     public T findLast(BiPredicate<T, Integer> predicate) {
-        for (int i = this.elements.length - 1; i >= 0; i--) {
+        for (int i = this.length - 1; i >= 0; i--) {
             if (predicate.test(this.elements[i], i)) {
                 return this.elements[i];
             }
@@ -208,7 +222,7 @@ public class JSArray<T extends Object> {
     }
 
     public int findLastIndex(BiPredicate<T, Integer> predicate) {
-        for (int i = this.elements.length - 1; i >= 0; i--) {
+        for (int i = this.length - 1; i >= 0; i--) {
             if (predicate.test(this.elements[i], i)) {
                 return i;
             }
@@ -217,7 +231,9 @@ public class JSArray<T extends Object> {
         return -1;
     }
 
-    public JSArray flat() {
+    // TODO: optimize this and include depth
+    @SuppressWarnings("unchecked")
+    public JSArray flat() throws SizeLimitExceededException {
         int dimension = this.getDimension();
 
         if (dimension <= 1) {
@@ -226,25 +242,24 @@ public class JSArray<T extends Object> {
 
         ArrayList<T> out = new ArrayList<>();
 
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < this.length; i++) {
             for (int j = 0; j < ((T[]) this.elements[i]).length; j++) {
                 out.add(((T[]) this.elements[i])[j]);
             }
         }
 
-        this.elements = (T[]) out.toArray(new Object[out.size()]);
+        T[] result = (T[]) out.toArray(new Object[out.size()]);
 
-        return this;
+        return new JSArray<>(result);
     }
 
-    /*
-     * public JSArray flatMap(BiFunction<T, Integer, ?> mapper) {
-     * return this.flat().map(mapper);
-     * }
-     */
+    @SuppressWarnings("unchecked")
+    public <U extends Object> JSArray flatMap(BiFunction<T, Integer, U> mapper) throws SizeLimitExceededException {
+        return new JSArray<>(this.elements).flat().map(mapper);
+    }
 
     public void forEach(BiConsumer<T, Integer> function) {
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < this.length; i++) {
             function.accept(this.elements[i], i);
         }
     }
@@ -254,32 +269,236 @@ public class JSArray<T extends Object> {
     }
 
     public int indexOf(T t) {
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < this.length; i++) {
             if (this.elements[i].equals(t)) {
                 return i;
             }
         }
-        
+
         return -1;
     }
 
-    public JSArray push(T item) throws SizeLimitExceededException {
-        for (int i = 0; i < this.elements.length; i++) {
-            if (elements[i] == null) {
-                elements[i] = item;
-                return this;
+    public String join(String defaultSeparator) {
+        String sep = ",";
+
+        if (defaultSeparator != null) {
+            sep = defaultSeparator;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < this.length; i++) {
+            builder.append(this.elements[i]);
+            if (i < this.length - 1) {
+                builder.append(sep);
             }
         }
 
-        throw new SizeLimitExceededException("Cannot push to array");
+        return builder.toString();
+    }
+
+    public int[] keys() {
+        int[] keys = new int[this.length];
+
+        for (int i = 0; i < this.length; i++) {
+            keys[i] = i;
+        }
+
+        return keys;
+    }
+
+    public int lastIndexOf(T t, int fromIndex) {
+        if (fromIndex < 0 || fromIndex >= this.length) {
+            throw new IllegalArgumentException("index must be between 0 and array length - 1");
+        }
+
+        for (int i = fromIndex; i >= 0; i--) {
+            if (this.elements[i].equals(t)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <U extends Object> JSArray map(BiFunction<T, Integer, U> mapper) throws SizeLimitExceededException {
+        U[] out = (U[]) new Object[this.length];
+
+        for (int i = 0; i < this.length; i++) {
+            out[i] = mapper.apply(this.elements[i], i);
+        }
+
+        return new JSArray<>((T[]) out);
+    }
+
+    public T pop() {
+        T popped = null;
+
+        if (this.length == 0) {
+            return popped;
+        }
+
+        @SuppressWarnings("unchecked")
+        T[] array = (T[]) new Object[this.length - 1];
+
+        for (int i = 0; i < this.length; i++) {
+            if (i == this.length - 1) {
+                popped = this.elements[i];
+                break;
+            }
+            array[i] = this.elements[i];
+
+        }
+
+        this.elements = array;
+        this.length--;
+
+        return popped;
+    }
+
+    public int push(@SuppressWarnings("unchecked") T... items) throws SizeLimitExceededException {
+        if (items.length > MAX_CAPACITY || this.length + items.length > MAX_CAPACITY) {
+            throw new SizeLimitExceededException("Cannot push to array, size exceeded");
+        }
+
+        this.elements = this.concat(items);
+        this.length = this.elements.length;
+        return this.length;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <U extends Object> JSArray reduce(TriFunction<U[], T, Integer, U[]> reducer, U[] initialValue)
+            throws SizeLimitExceededException {
+        U[] values = initialValue;
+
+        for (int i = 0; i < this.length; i++) {
+            values = reducer.apply(values, this.elements[i], i);
+        }
+
+        return new JSArray<>((T[]) values);
+    }
+
+    public double reduce(TriFunction<Double, T, Integer, Double> reducer, double initialValue) {
+        double value = initialValue;
+        for (int i = 0; i < this.length; i++) {
+            value = reducer.apply(value, this.elements[i], i);
+        }
+
+        return value;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <U extends Object> JSArray reduceRight(TriFunction<U[], T, Integer, U[]> reducer, U[] initialValue)
+            throws SizeLimitExceededException {
+        U[] values = initialValue;
+
+        for (int i = this.length - 1; i >= 0; i--) {
+            values = reducer.apply(values, this.elements[i], i);
+        }
+
+        return new JSArray<>((T[]) values);
+    }
+
+    public double reduceRight(TriFunction<Double, T, Integer, Double> reducer, double initialValue) {
+        double value = initialValue;
+        for (int i = this.length - 1; i >= 0; i--) {
+            value = reducer.apply(value, this.elements[i], i);
+        }
+
+        return value;
+    }
+
+    public JSArray reverse() throws SizeLimitExceededException {
+        @SuppressWarnings("unchecked")
+        T[] reversed = (T[]) new Object[this.length];
+
+        for (int i = 0; i < this.length; i++) {
+            reversed[i] = this.elements[this.length - 1 - i];
+        }
+
+        return new JSArray<>(reversed);
+    }
+
+    public T shift() {
+        T shifted = null;
+
+        if (this.length == 0) {
+            return shifted;
+        }
+
+        @SuppressWarnings("unchecked")
+        T[] out = (T[]) new Object[this.length - 1];
+
+        shifted = this.elements[0];
+        for (int i = 1; i < this.length; i++) {
+            out[i - 1] = this.elements[i];
+        }
+   
+        this.elements = out;
+        this.length--;
+
+        return shifted;
+    }
+
+    public JSArray slice(int start, int end) throws SizeLimitExceededException {
+        if (start >= end) {
+            throw new IllegalArgumentException("start index must be lower than end index");
+        }
+
+        ArrayList<T> out = new ArrayList<>();
+
+        for (int i = start; i < end; i++) {
+            out.add(this.elements[i]);
+        }
+
+        @SuppressWarnings("unchecked")
+        T[] result = (T[]) out.toArray(new Object[out.size()]);
+
+        return new JSArray<>(result);
+    }
+
+    public boolean some(BiFunction<T, Integer, Boolean> predicate) {
+        boolean isValid = false;
+
+        for (int i = 0; i < this.length; i++) {
+            isValid = predicate.apply(this.elements[i], i).booleanValue();
+            if (isValid) {
+                return isValid;
+            }
+        }
+
+        return isValid;
+    }
+
+    // TODO
+    public JSArray sort(BiFunction<T, T, Integer> sorting) {
+        for (int i = 1; i < this.length; i++) {
+            T a = this.elements[i];
+            T b = this.elements[i - 1];
+            int result = sorting.apply(a, b);
+            if (result < 0) {
+                T copy = a;
+                a = b;
+                b = copy;
+                continue;
+            }
+            if (result > 0) {
+                T copy = b;
+                b = a;
+                a = copy;
+                continue;
+            }
+        }
+        return this;
     }
 
     public String toString() {
         StringBuilder builder = new StringBuilder("[");
 
-        for (int i = 0; i < this.elements.length; i++) {
+        for (int i = 0; i < this.length; i++) {
             builder.append(this.elements[i]);
-            if (i < this.elements.length - 1) {
+            if (i < this.length - 1) {
                 builder.append(", ");
             }
         }
--- Main.java
+++ Main.java
@@ -1,36 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 import javax.naming.SizeLimitExceededException;
 
 public class Main {
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws SizeLimitExceededException {
         JSArray<String> array = new JSArray<>("T", "E", "S", "T");
         JSArray<String> array2 = new JSArray<>("A", "B", "C");
-        String[] array3 = JSArray.from("TEXT");
-        String[] array4 = JSArray.from(array3, (x) -> {
-            return x.toLowerCase();
+        JSArray<String> array3 = JSArray.from("TEXT");
+        JSArray<String> array4 = JSArray.from(array3, (x) -> {
+            return ((String) x).toLowerCase();
         });
-        JSArray<String[]> array5 = new JSArray<>(array3, array4);
+        JSArray<String[]> array5 = new JSArray<>(array3.toPrimitiveArray(), array4.toPrimitiveArray());
 
         System.out.println("First array: " + array);
         System.out.println("Second array: " + array2);
+        System.out.println("Thrid array: " + array3);
+        System.out.println("Fourth array: " + array4);
 
-        System.out.println("Thrid array: ");
-        for (String item : array3) {
-            System.out.println(item);
-        }
+        JSArray concatenated = array2.concat(array);
+        System.out.println("Concatenate second array with first: " + concatenated);
 
-        System.out.println("Fourth array: ");
-        for (String item : array4) {
-            System.out.println(item);
-        }
-
-        array2.concat(array);
-        System.out.println("Concatenate second array with first: " + array2);
-
-        array2.copyWithin(1, 3, 6);
-        System.out.println("Copy values within second array: " + array2);
+        JSArray copy = concatenated.copyWithin(1, 3, 6);
+        System.out.println("Copy values within concatenated array: " + copy);
 
         Iterator it = array2.entries();
         System.out.println("Iterate over second array");
@@ -42,13 +37,13 @@ public class Main {
             return elt.equals("T");
         }));
 
-        array2.fill("0", 2, 4);
-        System.out.println("Fill second array with '0' at index 2 to index 3: " + array2);
+        JSArray filled = concatenated.fill("0", 2, 4);
+        System.out.println("Fill concatenated array with '0' at index 2 to index 3: " + filled);
 
-        array2.filter((elt, i) -> {
+        JSArray filtered = array2.filter((elt, i) -> {
             return i % 2 == 0;
         });
-        System.out.println("Filter second array to retrieve only values with even index: " + array2);
+        System.out.println("Filter second array to retrieve only values with even index: " + filtered);
 
         String elt = array2.find((e, i) -> {
             return i == 2;
@@ -70,8 +65,8 @@ public class Main {
         });
         System.out.println("Find last index of 'T' in first array: " + lastIndex);
 
-        array5.flat();
-        System.out.println("Flat fifth array: " + array5);
+        JSArray flatted = array5.flat();
+        System.out.println("Flat fifth array: " + flatted);
 
         System.out.println("Lopping through second array to print each item with its matching index");
         array2.forEach((e, i) -> System.out.println("[" + e + ", " + i + "]"));
@@ -81,5 +76,67 @@ public class Main {
 
         int eltIndex = array2.indexOf("0");
         System.out.println("Find index of '0' in second array: " + eltIndex);
+
+        String defaultJoined = array2.join(null);
+        System.out.println("Join items with default separator in second array: " + defaultJoined);
+
+        String joined = array2.join("-");
+        System.out.println("Join items with custom separator in second array: " + joined);
+
+        int[] keys = array.keys();
+        System.out.println("Keys from first array");
+        for (int key : keys) {
+            System.out.println(key);
+        }
+
+        int lastIdx = array2.lastIndexOf("A", 2);
+        System.out.println("Get last index of 'A' of second array: " + lastIdx);
+
+        JSArray mapped = array2.map((e, i) -> {
+            return i;
+        });
+        System.out.println("Map second array to have indices instead of values: " + mapped);
+
+        String popped = array.pop();
+        System.out.println("Pop element from first array: " + popped + ", first array: " + array);
+
+        int length = array.push(new String[] { "H", "E", "L", "L", "O" });
+        System.out.println("Push elements into first array, new array: " + array + " (" + length + ")");
+
+        JSArray reduced = array.reduce((acc, curr, idx) -> {
+            ArrayList<String> list = new ArrayList<>(Arrays.asList(acc));
+            if (idx % 2 == 0) {
+                list.add(curr);
+            }
+            return list.toArray(new String[0]);
+        }, new String[0]);
+        System.out.println("Reduce first array to contains only element at which index is even: " + reduced);
+
+        JSArray<Double> array6 = new JSArray<>(1.0, 2.0, 3.0, 4.0, 5.0);
+        double reducedValue = array6.reduce((acc, curr, idx) -> {
+            return acc + curr;
+        }, 0);
+
+        System.out.println("Sum all numbers of the sixth array: " + reducedValue);
+
+        JSArray reversed = array6.reverse();
+        System.out.println("Reverse sixth array: " + reversed);
+
+        double shifted = array6.shift();
+        System.out.println("Shift head of the sixth array: " + shifted + ", sixth array: " + array6);
+
+        JSArray sliced = array6.slice(1, 3);
+        System.out.println("Slice sixth array from index 1 to index 2: " + sliced);
+
+        System.out.println("Sixth array contains at least one even value: " + array6.some((e, i) -> {
+            return e % 2 == 0;
+        }));
+
+        JSArray<Double> array7 = new JSArray<>(1.0, 2.0, 3.0, 4.0, 5.0);
+
+        JSArray sorted = array7.sort((a, b) -> {
+            return (int) (b - a);
+        });
+        System.out.println("Sorting seventh array descendant: " + sorted);
     }
 }
